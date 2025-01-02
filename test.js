085ade2f33f2d34454bd2aa3b8aa79e4cebb4b8a
atharvapatil65 <137687193+atharvapatil65@users.noreply.github.com>
2025-01-02 23:15:47 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,21 @@
+// const accountID=31252
+// let accountEmail="atharva@google.com"
+// var accountPassword="de34uh"
+
+// accountName="TravelGo"
+
+// console.log(accountName);
+
+// console.table([accountID, accountEmail, accountPassword, accountName])
+let n= 3
+for (i=0; i<n ; i++){
+console.log("atharva");
+}
+
+console.log(2 / 5);
+console.log(2 * 5);
+console.log(2 + 5);
+console.log(2 - 5);
+
+
+
